aa11220330b7cf0e0acf5feb3aa557c09334c223
Vansh <vanshbulani6@gmail.com>
2024-04-28 23:23:56 +0530

    adding auth.json

--- src/index.js
+++ src/index.js
@@ -1,51 +1,67 @@
-const express=require("express");
-const app=express(); //start express.js
-const path=require("path");
-const hbs=require("hbs");
-const collection=require("./mongodb")
+const express = require("express");
+const app = express(); //start express.js
+const path = require("path");
+const hbs = require("hbs");
+const collection = require("./mongodb");
+const fs = require('fs');
 
 
 const async = require("hbs/lib/async");
-const tempelatePath=path.join(__dirname,'../tempelates')
+const tempelatePath = path.join(__dirname, '../tempelates')
 app.use(express.json())
-app.set("view engine","hbs")
-app.set("views",tempelatePath)
-app.use(express.urlencoded({extended:false}))
+app.set("view engine", "hbs")
+app.set("views", tempelatePath)
+app.use(express.urlencoded({ extended: false }))
 
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.render("login")
 })
 
-app.get("/signup",(req,res)=>{
+app.get("/signup", (req, res) => {
     res.render("signup")
 })
 
-app.post("/signup",async(req,res)=>{
-    const data={
-        name:req.body.name,
-        password:req.body.password
+app.post("/signup", async (req, res) => {
+    const data = {
+        name: req.body.name,
+        password: req.body.password
     }
-    await collection.insertMany([data])
+    let jsonData = [];
+    try {
+        const existingData = fs.readFileSync('auth.json');
+        jsonData = JSON.parse(existingData);
+    } catch (error) {
+        console.log(error)
+    }
+
+    jsonData.push(data);
+    fs.writeFileSync('auth.json', JSON.stringify(jsonData, null, 2));
     res.render("home")
 
 })
-app.post("/login",async(req,res)=>{
-    
-   try{
-    const check=await collection.findOne({name:req.body.name})
-    if(check.password===req.body.password){
-        res.render("home")
-    }
-    else{
-        res.send("wrong passowrd")
-    }
-   }
-   catch{
-    res.send("wrong details")
-   }
 
-})
-app.listen(3000,()=>{
+app.post("/login", (req, res) => {
+    const username = req.body.name;
+    const password = req.body.password;
+
+    let jsonData = [];
+    try {
+        const existingData = fs.readFileSync('auth.json');
+        jsonData = JSON.parse(existingData);
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
+        return;
+    }
+
+    const user = jsonData.find(entry => entry.name === username && entry.password === password);
+    if (user) {
+        res.render("dashboard");
+    } else {
+        res.render("login", { error: "Invalid username or password" });
+    }
+});
+
+app.listen(3000, () => {
     console.log("port connected");
 })
